4eea41f5a523287f03a122f76932b6890085273f
5 <you@example.com>
2022-01-30 16:13:25 -0800

    initial commit

---
+++ new/First.java
@@ -0,0 +1,9 @@
+package new
+class First
+{
+public static void main(String[] args)
+{
+int k=(int)5.8;
+System.out.print(k);
+}
+}
---
+++ new/Num.java
@@ -0,0 +1,15 @@
+package new
+import java.util.Scanner;
+class Num
+{
+public static void main(String[] args)
+{
+Scanner myobj = new Scanner(System.in);
+System.out.println("Enter a num1");
+int num1 = myobj.nextInt();
+System.out.println("Enter a num2");
+int num2 = myobj.nextInt();
+System.out.println(num1+num2);
+}}
+
+
